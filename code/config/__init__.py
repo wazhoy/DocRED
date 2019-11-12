15b1296fc47cf9301579faea3dbad5f2fa5e1430
wangzhongyuan <ibukikonoha@outlook.com>
2019-11-12 19:36:47 +0800

    change init of config

--- code/config/__init__.py
+++ code/config/__init__.py
@@ -1,2 +1,7 @@
 from .Config import Config
+from .Config_part1 import Config as Config_part1
+from .Config_part2 import Config as Config_part2
+from .Config_part3 import Config as Config_part3
+from .Config_part4 import Config as Config_part4
+from .Config_part5 import Config as Config_part5
 from .EviConfig import EviConfig
